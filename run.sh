cbb5735c48a86504fc3052fbcc4f82a87fc04bb4
franchouille77 <36474420+franchouille77@users.noreply.github.com>
2025-05-01 23:54:00 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/usr/bin/with-contenv bashio
 
-cd /somfy/*
-pigpiod
+# Start pigpio daemon for GPIO access
+pigpiod -g -f -l &
+
+# Wait for pigpio daemon to initialize
 sleep 0.5
-pigs t
-./operateShutters.py -c /config/shutters.conf -a -m
+
+# Run the Pi-Somfy script with the configuration file
+cd /Pi-Somfy
+/venv/bin/python3 operateShutters.py -c /config/shutters.conf -a
